4d394188e7bdfaf372f5cb551a5fd9ce1a91e2e4
iasssy <iana.setrakova@johnabbottcollege.net>
2024-07-14 20:38:06 -0400

    added correct links from f3 BASE to header

--- website/tmp/2c4gm7lj24kks.2x98p6jsgu4gs.php
+++ website/tmp/2c4gm7lj24kks.2x98p6jsgu4gs.php
@@ -1,6 +1,6 @@
     <footer class="footer mt-auto py-3 bg-body-tertiary fixed-bottom">
       <div class="container">
-        <span class="text-body-secondary">Place sticky footer content here.</span>
+        <span class="text-body-secondary">Fixed bottom footer</span>
       </div>
     </footer>
 
