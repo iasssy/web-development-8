c5e7f1ac1ce233f107e790a84a50cea4907ebdb1
iasssy <iana.setrakova@johnabbottcollege.net>
2024-07-15 07:52:37 -0400

    added default title, made default title to be dynamically appended with current title

--- website/Controllers/Controller.php
+++ website/Controllers/Controller.php
@@ -1,9 +1,37 @@
 <?php 
-// patent controller class
+// parent controller class
 class Controller{
 
+  // Instance of the Fat-Free framework object, to be able to use it in any functions without using it as function argument
+  private $f3;
+
+   /**
+   * Constructor method
+   *
+   * This method is automatically called when an instance of the Controller class is created. 
+   *
+   * @param Base $f3 The Fat-Free Framework instance
+   */
   function __construct($f3){
+
+    $this->f3 = $f3;
+    // sets up a default page title 
     $f3->set('pageTitle', 'Task-IT');
+  }
+ 
+
+
+  public function setPageTitle($title){
+    $currentTitle = $this->f3->get('pageTitle');
+
+    $newTitle = $title;
+
+    if ($currentTitle != ""){
+        // append string
+        $newTitle .= " | ". $currentTitle;
+    }
+
+    $this->f3->set('pageTitle', $newTitle);
   }
 
 }
--- website/Controllers/PagesController.php
+++ website/Controllers/PagesController.php
@@ -9,7 +9,7 @@ class PagesController extends Controller{
      * @param Base $f3 The Fat-Free Framework instance
      */
     function homepage($f3){
-        $f3->set('pageTitle', 'Home - TASK-IT');
+        $this->setPageTitle('Home');
         $f3->set('pageDecription', 'TASK-IT: Simplify and organize your tasks effectively with our easy-to-use to-do list application.');
         echo Template::instance()->render('index.html');
     } 
@@ -20,8 +20,8 @@ class PagesController extends Controller{
      * 
      * @param Base $f3 The Fat-Free Framework instance
      */
-    function dashboard($f3){    
-        $f3->set('pageTitle', 'Dashboard - TASK-IT');
+    function dashboard($f3){  
+        $this->setPageTitle('Dashboard');
         $f3->set('pageDecription', 'Manage your tasks efficiently with TASK-IT intuitive Dashboard. Stay organized and focused on your goals with our powerful task management features.');   
         echo Template::instance()->render('dashboard.html');
     }
@@ -32,8 +32,8 @@ class PagesController extends Controller{
      * 
      * @param Base $f3 The Fat-Free Framework instance
      */
-    function login($f3){           
-        $f3->set('pageTitle', 'Login to TASK-IT');
+    function login($f3){   
+        $this->setPageTitle('Login');
         $f3->set('pageDecription', 'Log in to TASK-IT to access your personalized task management dashboard. Simplify your task management and stay organized with ease.');    
         echo Template::instance()->render('log-in.html');
     }
